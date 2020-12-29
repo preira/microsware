8c3c547cfda49db985c5cae4a255a54366797d14
joao.pereira <pereira.joao@gmail.com>
2020-12-29 13:05:16 +0000

    add log to api

--- src/api/api.ts
+++ src/api/api.ts
@@ -32,13 +32,13 @@ export class API {
     }
 
     route(path: string) {
-        return new MSWRoute(path, this.server)
+        return new MSWRoute(path, this.server, this.logger)
     }
 
     run() {
         // rest of the code remains same
         this.server.listen(this.config.server.httpport, () => {
-          console.log(`⚡️[server]: Server now is running at https://${this.config.server.hostname}:${this.config.server.httpport}`);
+            this.logger.info(`⚡️[server]: Server now is running at https://${this.config.server.hostname}:${this.config.server.httpport}`);
         })
       }
     
@@ -50,47 +50,57 @@ export class API {
  */
 export class MSWRoute {
     route: IRoute
+    logger: Logger
     
-    constructor(path: string, server: Express) {
+    constructor(path: string, server: Express, logger: Logger) {
+        this.logger = logger
         this.route = server.route(path)
     }
 
     all(...handlers: RequestHandler<any, any, any, any>[]): MSWRoute {
+        this.logger.info(`Adding route handler 'ALL' for '${this.route.path}'`)
         this.route = this.route.all(handlers)
         return this
     }
     
     get(...handlers: RequestHandler<any, any, any, any>[]): MSWRoute {
+        this.logger.info(`Adding route handler 'GET' for '${this.route.path}'`)
         this.route = this.route.get(handlers)
         return this
     }
     
     post(...handlers: RequestHandler<any, any, any, any>[]): MSWRoute {
+        this.logger.info(`Adding route handler 'POST' for '${this.route.path}'`)
         this.route = this.route.post(handlers)
         return this
     }
     
     put(...handlers: RequestHandler<any, any, any, any>[]): MSWRoute {
+        this.logger.info(`Adding route handler 'PUT' for '${this.route.path}'`)
         this.route = this.route.put(handlers)
         return this
     }
     
     delete(...handlers: RequestHandler<any, any, any, any>[]): MSWRoute {
+        this.logger.info(`Adding route handler 'DELETE' for '${this.route.path}'`)
         this.route = this.route.delete(handlers)
         return this
     }
     
     patch(...handlers: RequestHandler<any, any, any, any>[]): MSWRoute {
+        this.logger.info(`Adding route handler 'PATCH' for '${this.route.path}'`)
         this.route = this.route.patch(handlers)
         return this
     }
     
     options(...handlers: RequestHandler<any, any, any, any>[]): MSWRoute {
+        this.logger.info(`Adding route handler 'OPTION' for '${this.route.path}'`)
         this.route = this.route.options(handlers)
         return this
     }
 
     head(...handlers: RequestHandler<any, any, any, any>[]): MSWRoute {
+        this.logger.info(`Adding route handler 'HEAD' for '${this.route.path}'`)
         this.route = this.route.head(handlers)
         return this
     }
